2489e91ef2ec952cc0807b698d829c5532b8b30d
kennycallado <kennycallado@hotmail.com>
2023-08-28 19:53:53 +0200

    ADD Agrega derive Eq... para testing

--- src/types.rs
+++ src/types.rs
@@ -10,20 +10,20 @@ pub struct Client {
     pub state: ClientState,
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
 pub struct ClientState {
     pub memory: usize,
     pub tasks: usize,
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
 pub enum Action {
     Join((String, std::time::SystemTime)),
     Check((String, ClientState)),
     // Test(u64),
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
 pub struct Message {
     pub action: Action,
 }
